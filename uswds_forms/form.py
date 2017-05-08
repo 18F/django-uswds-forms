249f3254771fe487a9d8bb83d5b26fd1ed15148d
Atul Varma <varmaa@gmail.com>
2017-05-08 16:31:29 -0400

    Add UswdsForm.as_fieldsets() convenience method.

--- uswds_forms/form.py
+++ uswds_forms/form.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.template.loader import render_to_string
 
 from .errors import UswdsErrorList
 
@@ -8,3 +9,8 @@ class UswdsForm(forms.Form):
         if 'error_class' not in kwargs:
             kwargs['error_class'] = UswdsErrorList
         super().__init__(*args, **kwargs)
+
+    def as_fieldsets(self):
+        return render_to_string('uswds_forms/form_as_fieldsets.html', {
+            'form': self,
+        })
