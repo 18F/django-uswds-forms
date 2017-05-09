b2fc3ff06db14ffdc6074c7a86923733e26ba2a6
Atul Varma <varmaa@gmail.com>
2017-05-09 08:14:45 -0400

    Add tests to ensure widgets use usa-unstyled-list.

--- uswds_forms/tests/test_widgets.py
+++ uswds_forms/tests/test_widgets.py
@@ -1,7 +1,33 @@
 import datetime
+from django import forms
+
 from uswds_forms.date import SplitDateWidget
+from uswds_forms import UswdsCheckboxSelectMultiple, UswdsRadioSelect
+
 
 def test_split_date_widget_decompress():
     w = SplitDateWidget()
     d = datetime.date(2017, 5, 4)
     assert w.decompress(d) == [2017, 5, 4]
+
+
+def test_checkbox_select_multiple_uses_unstyled_list():
+    class MyForm(forms.Form):
+        checkboxes = forms.MultipleChoiceField(
+            widget=UswdsCheckboxSelectMultiple,
+            choices=(('one', 'One'), ('two', 'Two')),
+        )
+
+    # Note that this will fail if Django < 1.11.1!
+    assert 'usa-unstyled-list' in MyForm().as_p()
+
+
+def test_radio_select_uses_unstyled_list():
+    class MyForm(forms.Form):
+        radios = forms.ChoiceField(
+            widget=UswdsRadioSelect,
+            choices=(('one', 'One'), ('two', 'Two')),
+        )
+
+    # Note that this will fail if Django < 1.11.1!
+    assert 'usa-unstyled-list' in MyForm().as_p()
