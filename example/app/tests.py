d27e26350306b159a0d74ab71434d8b8f7b01ece
Atul Varma <varmaa@gmail.com>
2017-05-10 17:26:29 -0400

    Add a trivial test for the example app.

--- example/app/tests.py
+++ example/app/tests.py
@@ -1,1 +1,7 @@
-# Create your tests here.
+from django.test import TestCase
+
+
+class AppTests(TestCase):
+    def test_home_loads(self):
+        res = self.client.get('/')
+        self.assertEqual(res.status_code, 200)
