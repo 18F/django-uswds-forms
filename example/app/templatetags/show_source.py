8687da6f6c9db8b5a8c434302a5b43dc87b582cc
Atul Varma <varmaa@gmail.com>
2017-05-11 19:26:18 -0400

    Make pygments an optional dependency for example app.

--- example/app/templatetags/show_source.py
+++ example/app/templatetags/show_source.py
@@ -1,10 +1,14 @@
 from pathlib import Path
 from django import template
 from django.utils.safestring import SafeString
-from pygments import highlight
-from pygments.lexers import PythonLexer
-from pygments.lexers.templates import HtmlDjangoLexer
-from pygments.formatters import HtmlFormatter
+
+try:
+    from pygments import highlight
+    from pygments.lexers import PythonLexer
+    from pygments.lexers.templates import HtmlDjangoLexer
+    from pygments.formatters import HtmlFormatter
+except ImportError:
+    highlight = None
 
 MY_DIR = Path(__file__).resolve().parent
 APP_DIR = MY_DIR.parent
@@ -14,19 +18,27 @@ TEMPLATES_DIR = APP_DIR / 'templates' / 'examples'
 register = template.Library()
 
 
-def get_formatter():
-    return HtmlFormatter(noclasses=True, style='trac')
+def render_source(contents, filetype):
+    if highlight is None:
+        return contents
+    else:
+        formatter = HtmlFormatter(noclasses=True, style='trac')
+        if filetype == 'html+django':
+            lexer = HtmlDjangoLexer()
+        elif filetype == 'python':
+            lexer = PythonLexer()
+        else:
+            raise ValueError('unknown filetype: {}'.format(filetype))
+        return SafeString(highlight(contents, lexer, formatter))
 
 
 @register.simple_tag(takes_context=True)
 def show_template_source(context, filename):
     with open(str(TEMPLATES_DIR / filename)) as f:
-        return SafeString(highlight(f.read(), HtmlDjangoLexer(),
-                          get_formatter()))
+        return render_source(f.read(), 'html+django')
 
 
 @register.simple_tag(takes_context=True)
 def show_python_source(context, filename):
     with open(str(PY_DIR / filename)) as f:
-        return SafeString(highlight(f.read(), PythonLexer(),
-                          get_formatter()))
+        return render_source(f.read(), 'python')
