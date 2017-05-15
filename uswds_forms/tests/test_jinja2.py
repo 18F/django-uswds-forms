382cec4411a647aff161a17c65e3e177a2a1c8ec
Atul Varma <varmaa@gmail.com>
2017-05-15 15:49:27 -0400

    Refactor test_jinja2.py.

--- uswds_forms/tests/test_jinja2.py
+++ uswds_forms/tests/test_jinja2.py
@@ -6,31 +6,28 @@ JINJA2_DIR = APP_DIR / 'jinja2' / 'uswds_forms'
 TEMPLATES_DIR = APP_DIR / 'templates' / 'uswds_forms'
 
 
-def test_date_template_is_identical():
+def assert_templates_are_identical(name):
     '''
-    This template uses the subset of syntax shared by Jinja2 and
+    Some of our templates use the subset of syntax shared by Jinja2 and
     Django Templates, so the two versions of it should be identical.
+
+    Note that this assertion is currently here to ensure that either
+    backend doesn't lag behind the other; however, if a real need
+    arises for the templates to diverge, tests using this assertion
+    can be removed.
     '''
 
-    assert (JINJA2_DIR / 'date.html').read_text() == \
-           (TEMPLATES_DIR / 'date.html').read_text()
+    assert (JINJA2_DIR / name).read_text() == \
+           (TEMPLATES_DIR / name).read_text()
+
+
+def test_date_template_is_identical():
+    assert_templates_are_identical('date.html')
 
 
 def test_input_option_template_is_identical():
-    '''
-    This template uses the subset of syntax shared by Jinja2 and
-    Django Templates, so the two versions of it should be identical.
-    '''
-
-    assert (JINJA2_DIR / 'input_option.html').read_text() == \
-           (TEMPLATES_DIR / 'input_option.html').read_text()
+    assert_templates_are_identical('input_option.html')
 
 
 def test_fieldset_template_is_identical():
-    '''
-    This template uses the subset of syntax shared by Jinja2 and
-    Django Templates, so the two versions of it should be identical.
-    '''
-
-    assert (JINJA2_DIR / 'fieldset.html').read_text() == \
-           (TEMPLATES_DIR / 'fieldset.html').read_text()
+    assert_templates_are_identical('fieldset.html')
